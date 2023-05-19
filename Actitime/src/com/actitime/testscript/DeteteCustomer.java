05e68eda75cc48783498ead06af529d309bc652c
91728 <91728@192.168.1.11>
2023-05-19 13:40:36 +0530

    Deteting

---
+++ Actitime/src/com/actitime/testscript/DeteteCustomer.java
@@ -0,0 +1,12 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DeteteCustomer {
+	@Test
+	public void testDeleteCustomer() {
+		Reporter.log("Deleting",true);
+	}
+
+}
